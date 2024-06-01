232fd00595c12f7d2c67cf83a9726868904cc11c
ICEYSELF <icey@icey.tech>
2024-06-02 01:26:16 +0800

    test

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <Windows.h>
 
 #include "global.h"
+#include "fileutil.h"
+#include "strutil.h"
 
 int WINAPI WinMain(HINSTANCE hInstance,
                    HINSTANCE hPrevInstance,
@@ -12,5 +14,21 @@ int WINAPI WinMain(HINSTANCE hInstance,
     (VOID)lpszCmdLine;
     (VOID)nCmdShow;
 
+    LPSTR* arrlpszLines = ReadFileLines(L"map/map-site.txt");
+    if (arrlpszLines == NULL) {
+        return 1;
+    }
+
+    for (int i = 0; arrlpszLines[i] != NULL; i++) {
+        LPWSTR lpszLine = U8StringToWide(arrlpszLines[i]);
+        if (lpszLine == NULL) {
+            return 1;
+        }
+
+        MessageBoxW(NULL, lpszLine, L"Line", MB_OK);
+        HeapFree(GetProcessHeap(), 0, lpszLine);
+    }
+
+    FreeFileLines(arrlpszLines);
     return 0;
 }
